392b4353f2456f335f7d23eeffaffa66f4977c28
jinjianming <57084209+jinjianming@users.noreply.github.com>
2023-12-22 10:55:27 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,6 @@ export S3_ACL=${S3_ACL:-private}
 
 mkdir -p ${MNT_POINT}
 
-if [ "$IAM_ROLE" == "none" ]; then
   export AWSACCESSKEYID=${AWSACCESSKEYID:-$AWS_KEY}
   export AWSSECRETACCESSKEY=${AWSSECRETACCESSKEY:-$AWS_SECRET_KEY}
 
@@ -16,9 +15,5 @@ if [ "$IAM_ROLE" == "none" ]; then
   chmod 0400 /etc/passwd-s3fs
 
   echo 'IAM_ROLE is not set - mounting S3 with credentials from ENV'
-  /usr/bin/s3fs  ${S3_BUCKET} ${MNT_POINT} -d -d -f -o endpoint=${S3_REGION},allow_other,retries=5
+  /usr/bin/s3fs -o url=${URL} -o allow_other -o no_check_certificate -o use_path_request_style -o umask=000 ${S3_BUCKET} ${MNT_POINT}
   echo 'started...'
-else
-  echo 'IAM_ROLE is set - using it to mount S3'
-  /usr/bin/s3fs ${S3_BUCKET} ${MNT_POINT} -d -d -f -o endpoint=${S3_REGION},iam_role=${IAM_ROLE},allow_other,retries=5
-fi
